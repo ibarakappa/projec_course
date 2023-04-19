1e801fff007b9490ec6a79459209c4032836bc1c
DESKTOP-RRPRPE7\Lenovo <yz1410aa@gmail.com>
2023-04-20 00:40:17 +0800

    拯救個人專題第一步

---
+++ src/test/java/com/example/project_course/ProjectCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
